3b99a7638cf42fd3d6c3a877139a42cba7858a9b
CaeptnHero <63815774+CaeptnHero@users.noreply.github.com>
2020-10-28 15:07:03 +0100

    Add Login/Register-Window

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,26 @@
 package application;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javax.swing.*;
+
 public class MainController {
-	
+
+	@FXML
+	private TextField tfName;
+
+	@FXML
+	private JPasswortField pfPasswort;
+
+	@FXML
+	private Button btLogin;
+
+	@FXML
+	private void handleButtonLoginAction(ActionEvent event) {
+		System.out.println(tfName.getText() + " " + pfPasswort.getText());
+	}
+
 }
