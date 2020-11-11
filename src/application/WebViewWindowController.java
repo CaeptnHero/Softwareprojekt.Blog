11c0beb0ab36da2f047571785d130652a69ae53d
IsaakDl <Daniel.Isaak@pm.me>
2020-11-11 14:18:10 +0100

    added primitive website with some css and js

--- src/application/WebViewWindowController.java
+++ src/application/WebViewWindowController.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,8 +18,14 @@ public class WebViewWindowController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
+		File f = new File("www\\index.html");
 		WebEngine engine = webView.getEngine();
-		engine.load("http://www.i5ucc.me");
+		try {
+			engine.load(f.toURI().toURL().toString());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void setDBConnection(DBConnection dbc) {
---
+++ www/js/main.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    
+    test()
+}
+
+async function test() {
+    while (true) {
+        var h1 = this.document.querySelector('h1');
+        h1.style.cssText = "color: #" + Math.floor(Math.random()*16777215).toString(16);
+        await Sleep(1000);
+    }
+}
+
+function Sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
