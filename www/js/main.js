f3603602c4096f9701f3e8f9ef3551a1c48861e7
Sharagran <filipmaas@googlemail.com>
2020-11-26 18:49:11 +0100

    Frühjahrsputz

--- www/js/main.js
+++ www/js/main.js
@@ -1,26 +1,18 @@
 window.onload = function() {
     // on window loaded
-
+    clearArticles();
 }
 
 window.onerror = function (msg, url, line) {
     bridge.errorLog(msg, url, line);
 }
 
-
-
-/*
-function fill(art) {
-    document.getElementById("ti1").innerHTML = art["titel"];
-    document.getElementById("te1").innerHTML = art["text"];
-}
-*/
-
 function fill(art, s) {
+    // document.getElementById("titel"+ s).innerHTML = art["titel"];
+    // document.getElementById("text" + s).innerHTML = art["text"];
 
-
-    document.getElementById("titel"+ s).innerHTML = art["titel"];
-    document.getElementById("text" + s).innerHTML = art["text"];
+    //FIXME: tmp fix
+    displayArticle(-1, art.titel, art.text);
 }
 
 function ready(){
@@ -35,36 +27,47 @@ function addP(){
 
 // Funktion um weitere Seiten hinzuzufügen
 function Page(number){
-
     for (i = 1; i <=number; i++ ) {
-        var node = document.createElement("LI");
+        var node = document.createElement("li");
         var textnode = document.createTextNode(i);
         node.id = i;
         node.appendChild(textnode);
-        document.getElementById("page").appendChild(node);
+        document.querySelector("footer > nav > ul").appendChild(node);
         document.getElementById(i).innerHTML = "<li onclick='bridge.fillWeb(" + i + ")'><a>" + i + "</a></li>" ; // Anpassen sobald das Layout fertig. Als Beispiel <div class="message">Add Message<br>Title: <input type="text usw.
 
     }
 }
 
 function createArticle() {
-    let title = document.querySelector('input[name="tname"]').value;
-    let text = document.querySelector('input[name="ttext"]').value;
+    let title = document.getElementById('article-title').value;
+    let text = document.getElementById('article-text').value;
     bridge.createArticle(title, text);
 }
 
 //Funktion zum anzeigen von Artikeln aus DB
-function displayArticle(id, title, text) {
-    //TODO: Artikel element erstellen und mit übergebenen werten füllen
+function displayArticle(ID, Title, Text) {
+    let article = document.createElement('article');
+    article.id = ID;
+    article.innerHTML =
+        `<h2 id="titel2">${Title}</h2>
+         <p id="text2">${Text}</p>
+         <button class="button button1">Kommentieren</button>
+         <button class="button button2">Löschen</button>`;
+
+    document.getElementById("article-section").appendChild(article);
 }
 
 //Funktion zum hinzufügen von kommentaren in webview & in die DB
 function addKommentar(beitragID, kommentarText) {
-    //TODO: kommentar einen beitrag hinzufügen
+    document.getElementById(beitragID.toString()).app
 }
 
 function deleteArticle(id) {
     //TODO: kommentar aus view & DB löschen
+}
+
+function clearArticles() {
+    document.getElementById('article-section').innerHTML = '';
 }
 
 class Beitrag {
